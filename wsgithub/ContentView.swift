50d566c20251d58645191639bfd9dba42e14b6c7
claudiascognamiglio33 <scognamiglioclaudia33@gmail.com>
2024-11-04 15:18:05 +0100

    Initial Commit

---
+++ wsgithub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wsgithub
+//
+//  Created by Claudia Scognamiglio  on 04/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wsgithub/wsgithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  wsgithubApp.swift
+//  wsgithub
+//
+//  Created by Claudia Scognamiglio  on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct wsgithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
